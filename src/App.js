d8223e67996857f1dbb3ec4a5b3e56e0178855ec
Raul Victor <31050006+Raul772@users.noreply.github.com>
2022-12-23 02:47:30 -0300

    First Code Update

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import "./app.css";
+import Tasks from "./components/Tasks.js";
+
+const App = () => {
+	let [tasks, setTasks] = useState(
+		{
+			id: "1",
+			title: "Estudar Programação",
+			completed: false
+		},
+		{
+			id: "2",
+			title: "Ler Livros",
+			completed: true
+		});
+    return (
+				<>
+					<div className="container">
+						<Tasks tasks={tasks}/>
+					</div>;
+				</>
+			);
+}
+
+export default App;
