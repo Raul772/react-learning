091d549e4d786ee9ef6513ce7eb6b2b4b205ae14
Raul Victor <31050006+Raul772@users.noreply.github.com>
2023-01-02 18:58:37 -0300

    Add an add-task feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
 import "./app.css";
-import Tasks from "./components/Tasks.js";
+import Tasks from "./components/Tasks.jsx";
+import AddTask from "./components/AddTask.jsx"
+
+import {v4 as uuidv4} from 'uuid';
 
 const App = () => {
+
 	let [tasks, setTasks] = useState([
 		{
 			id: "1",
@@ -14,21 +18,28 @@ const App = () => {
 			id: "2",
 			title: "Ler Livros",
 			completed: true
-		},
-		{
-			id: "3",
-			title: "Ler Livros",
-			completed: true
 		}
 	]);
+
+
+	const handleTaskAddition = (taskTitle) => {
+		const newTasks = [...tasks, {
+			title: taskTitle,
+			id: uuidv4(),
+			completed: false
+		}];
+
+		setTasks(newTasks);
+	}
+
 
     return (
 		<>
 			<div className="container">
+				<AddTask handleTaskAddition={handleTaskAddition} />
 				<Tasks tasks={tasks}/>
 			</div>
-		</>
-			);
+		</>);
 };
 
 export default App;
