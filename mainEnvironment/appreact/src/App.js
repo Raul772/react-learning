523705f3cf19effec0360674658c8664b18b6e38
raul772 <raulvictor772@gmail.com>
2023-06-07 15:46:30 -0300

    Add atividade 1

--- mainEnvironment/appreact/src/App.js
+++ mainEnvironment/appreact/src/App.js
@@ -1,9 +1,49 @@
+const luana = {
+    cliente: "Luana",
+    idade: 27,
+    compras: [
+        { nome: "Notebook", preco: "R$ 2500" },
+        { nome: "Geladeira", preco: "R$ 3000" },
+        { nome: "Smartphone", preco: "R$ 1500" },
+    ],
+    ativa: true,
+};
+
+const mario = {
+    cliente: "Mario",
+    idade: 31,
+    compras: [
+        { nome: "Notebook", preco: "R$ 2500" },
+        { nome: "Geladeira", preco: "R$ 3000" },
+        { nome: "Smartphone", preco: "R$ 1500" },
+        { nome: "Guitarra", preco: "R$ 3500" },
+    ],
+    ativa: false,
+};
+
 const App = () => {
-	return (
-		<div>
-			Meu App
-		</div>
-	);
-}
+    const dados = luana;
+
+    let total = dados.compras
+        .map((item) => Number(item.preco.replace(/[A-Z]*[!-&]* /, "")))
+        .reduce((a, b) => a + b);
+
+    return (
+        <div>
+            <p>Nome: {dados.cliente}</p>
+            <p>Idade: {dados.idade}</p>
+            <p>
+                Situação:{' '}
+                <span
+                    style={{ color: dados.ativa ?  "green"  :  "red" }}
+                >
+                    {dados.ativa ? "Ativa" : "Inativa"}
+                </span>
+            </p>
+            <p>Total Gasto: R$ {total}</p>
+            {total > 10000 && <p>Você está gastando muito!</p>}
+        </div>
+    );
+};
 
 export default App;
